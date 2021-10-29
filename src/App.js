0c20f52270d5b9ac81368e2c167bedc1b1f68d06
f_baldomero <baldomero.fernandez@gmail.com>
2021-10-29 18:54:32 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://orangevalleycaa.org/api/videos").then(
+        (response) => response.json()
+      );
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Videos</h1>
       </header>
+      {data.map((video,id) =>(
+        <div key={id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls 
+                 src ={video.video_url}
+                 poster={video.thumbnail}></video>
+        </div>
+      ))}
     </div>
   );
 }
